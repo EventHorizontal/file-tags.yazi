3b999b6949b355f02ecaaa46ef198a4d08a2bddf
EventHorizontal <doubleedgedsword112@gmail.com>
2025-05-21 12:47:51 +0600

    Added obtaining currently selected/hovered files

--- main.lua
+++ main.lua
@@ -1,24 +1,38 @@
 local M = {}
 
-M.entry = function(_, job)
+local selected_or_hovered = ya.sync(function()
+	local tab, urls = cx.active, {}
+	for _, u in pairs(tab.selected) do
+		urls[#urls + 1] = u
+	end
+	if #urls == 0 and tab.current.hovered then
+		urls[1] = tab.current.hovered.url
+	end
+	return urls
+end)
+M.entry = function(self, job)
 	local action = job.args[1]
 
-	local value, event = ya.input({
-		title = "tag name",
-		position = {
-			"center",
-			w = 30,
-		},
-	})
+	-- local value, event = ya.input({
+	-- 	title = "tag name",
+	-- 	position = {
+	-- 		"center",
+	-- 		w = 30,
+	-- 	},
+	-- })
 
-	ya.notify({
-		title = "File Tag",
-		content = "Registered tag '" .. value .. "'",
-		timeout = 6.5,
-		-- level = "info",
-	})
+	local selection = selected_or_hovered()
+
+	for _, url in ipairs(selection) do
+		ya.notify({
+			title = "File Tag",
+			content = "Found file '" .. url .. "'",
+			timeout = 6.5,
+			-- level = "info",
+		})
+	end
 end
 
-M.setup = function(state, args) end
+M.setup = function(state, opts) end
 
 return M
