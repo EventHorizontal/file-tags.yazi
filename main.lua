4fbe8b302260aab6d3f21d5eb0404a9e44ceccce
EventHorizontal <doubleedgedsword112@gmail.com>
2025-05-22 08:51:04 +0600

    Moved json code into main.lua because plugins can only have one file.

--- main.lua
+++ main.lua
@@ -1,17 +1,356 @@
 local M = {}
 
-local json = require("./json")
+-- TODO: Hardcoded path for now
 local defaultLocation = "/home/cipher/Desktop/"
 
-local toggle_ui = ya.sync(function(self)
-	if self.children then
-		Modal:children_remove(self.children)
-		self.children = nil
-	else
-		self.children = Modal:children_add(self, 10)
+-- Plugins seem to have to be a single file so I'm dividing this
+-- file into several regions for organisation.
+
+-- Json Parsing #########################################################
+-- credit to rxi/json.lua for the json encoding and decoding code
+
+local json = { _version = "0.1.2" }
+
+-- Encode
+
+local encode
+
+local escape_char_map = {
+	["\\"] = "\\",
+	['"'] = '"',
+	["\b"] = "b",
+	["\f"] = "f",
+	["\n"] = "n",
+	["\r"] = "r",
+	["\t"] = "t",
+}
+
+local escape_char_map_inv = { ["/"] = "/" }
+for k, v in pairs(escape_char_map) do
+	escape_char_map_inv[v] = k
+end
+
+local function escape_char(c)
+	return "\\" .. (escape_char_map[c] or string.format("u%04x", c:byte()))
+end
+
+local function encode_nil(val)
+	return "null"
+end
+
+local function encode_table(val, stack)
+	local res = {}
+	stack = stack or {}
+
+	-- Circular reference?
+	if stack[val] then
+		error("circular reference")
 	end
-	ya.render()
-end)
+
+	stack[val] = true
+
+	if rawget(val, 1) ~= nil or next(val) == nil then
+		-- Treat as array -- check keys are valid and it is not sparse
+		local n = 0
+		for k in pairs(val) do
+			if type(k) ~= "number" then
+				error("invalid table: mixed or invalid key types")
+			end
+			n = n + 1
+		end
+		if n ~= #val then
+			error("invalid table: sparse array")
+		end
+		-- Encode
+		for i, v in ipairs(val) do
+			table.insert(res, encode(v, stack))
+		end
+		stack[val] = nil
+		return "[" .. table.concat(res, ",") .. "]"
+	else
+		-- Treat as an object
+		for k, v in pairs(val) do
+			if type(k) ~= "string" then
+				error("invalid table: mixed or invalid key types")
+			end
+			table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
+		end
+		stack[val] = nil
+		return "{" .. table.concat(res, ",") .. "}"
+	end
+end
+
+local function encode_string(val)
+	return '"' .. val:gsub('[%z\1-\31\\"]', escape_char) .. '"'
+end
+
+local function encode_number(val)
+	-- Check for NaN, -inf and inf
+	if val ~= val or val <= -math.huge or val >= math.huge then
+		error("unexpected number value '" .. tostring(val) .. "'")
+	end
+	return string.format("%.14g", val)
+end
+
+local type_func_map = {
+	["nil"] = encode_nil,
+	["table"] = encode_table,
+	["string"] = encode_string,
+	["number"] = encode_number,
+	["boolean"] = tostring,
+}
+
+encode = function(val, stack)
+	local t = type(val)
+	local f = type_func_map[t]
+	if f then
+		return f(val, stack)
+	end
+	error("unexpected type '" .. t .. "'")
+end
+
+function json.encode(val)
+	return (encode(val))
+end
+
+-- Decode
+
+local parse
+
+local function create_set(...)
+	local res = {}
+	for i = 1, select("#", ...) do
+		res[select(i, ...)] = true
+	end
+	return res
+end
+
+local space_chars = create_set(" ", "\t", "\r", "\n")
+local delim_chars = create_set(" ", "\t", "\r", "\n", "]", "}", ",")
+local escape_chars = create_set("\\", "/", '"', "b", "f", "n", "r", "t", "u")
+local literals = create_set("true", "false", "null")
+
+local literal_map = {
+	["true"] = true,
+	["false"] = false,
+	["null"] = nil,
+}
+
+local function next_char(str, idx, set, negate)
+	for i = idx, #str do
+		if set[str:sub(i, i)] ~= negate then
+			return i
+		end
+	end
+	return #str + 1
+end
+
+local function decode_error(str, idx, msg)
+	local line_count = 1
+	local col_count = 1
+	for i = 1, idx - 1 do
+		col_count = col_count + 1
+		if str:sub(i, i) == "\n" then
+			line_count = line_count + 1
+			col_count = 1
+		end
+	end
+	error(string.format("%s at line %d col %d", msg, line_count, col_count))
+end
+
+local function codepoint_to_utf8(n)
+	-- http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=iws-appendixa
+	local f = math.floor
+	if n <= 0x7f then
+		return string.char(n)
+	elseif n <= 0x7ff then
+		return string.char(f(n / 64) + 192, n % 64 + 128)
+	elseif n <= 0xffff then
+		return string.char(f(n / 4096) + 224, f(n % 4096 / 64) + 128, n % 64 + 128)
+	elseif n <= 0x10ffff then
+		return string.char(f(n / 262144) + 240, f(n % 262144 / 4096) + 128, f(n % 4096 / 64) + 128, n % 64 + 128)
+	end
+	error(string.format("invalid unicode codepoint '%x'", n))
+end
+
+local function parse_unicode_escape(s)
+	local n1 = tonumber(s:sub(1, 4), 16)
+	local n2 = tonumber(s:sub(7, 10), 16)
+	-- Surrogate pair?
+	if n2 then
+		return codepoint_to_utf8((n1 - 0xd800) * 0x400 + (n2 - 0xdc00) + 0x10000)
+	else
+		return codepoint_to_utf8(n1)
+	end
+end
+
+local function parse_string(str, i)
+	local res = ""
+	local j = i + 1
+	local k = j
+
+	while j <= #str do
+		local x = str:byte(j)
+
+		if x < 32 then
+			decode_error(str, j, "control character in string")
+		elseif x == 92 then -- `\`: Escape
+			res = res .. str:sub(k, j - 1)
+			j = j + 1
+			local c = str:sub(j, j)
+			if c == "u" then
+				local hex = str:match("^[dD][89aAbB]%x%x\\u%x%x%x%x", j + 1)
+					or str:match("^%x%x%x%x", j + 1)
+					or decode_error(str, j - 1, "invalid unicode escape in string")
+				res = res .. parse_unicode_escape(hex)
+				j = j + #hex
+			else
+				if not escape_chars[c] then
+					decode_error(str, j - 1, "invalid escape char '" .. c .. "' in string")
+				end
+				res = res .. escape_char_map_inv[c]
+			end
+			k = j + 1
+		elseif x == 34 then -- `"`: End of string
+			res = res .. str:sub(k, j - 1)
+			return res, j + 1
+		end
+
+		j = j + 1
+	end
+
+	decode_error(str, i, "expected closing quote for string")
+end
+
+local function parse_number(str, i)
+	local x = next_char(str, i, delim_chars)
+	local s = str:sub(i, x - 1)
+	local n = tonumber(s)
+	if not n then
+		decode_error(str, i, "invalid number '" .. s .. "'")
+	end
+	return n, x
+end
+
+local function parse_literal(str, i)
+	local x = next_char(str, i, delim_chars)
+	local word = str:sub(i, x - 1)
+	if not literals[word] then
+		decode_error(str, i, "invalid literal '" .. word .. "'")
+	end
+	return literal_map[word], x
+end
+
+local function parse_array(str, i)
+	local res = {}
+	local n = 1
+	i = i + 1
+	while 1 do
+		local x
+		i = next_char(str, i, space_chars, true)
+		-- Empty / end of array?
+		if str:sub(i, i) == "]" then
+			i = i + 1
+			break
+		end
+		-- Read token
+		x, i = parse(str, i)
+		res[n] = x
+		n = n + 1
+		-- Next token
+		i = next_char(str, i, space_chars, true)
+		local chr = str:sub(i, i)
+		i = i + 1
+		if chr == "]" then
+			break
+		end
+		if chr ~= "," then
+			decode_error(str, i, "expected ']' or ','")
+		end
+	end
+	return res, i
+end
+
+local function parse_object(str, i)
+	local res = {}
+	i = i + 1
+	while 1 do
+		local key, val
+		i = next_char(str, i, space_chars, true)
+		-- Empty / end of object?
+		if str:sub(i, i) == "}" then
+			i = i + 1
+			break
+		end
+		-- Read key
+		if str:sub(i, i) ~= '"' then
+			decode_error(str, i, "expected string for key")
+		end
+		key, i = parse(str, i)
+		-- Read ':' delimiter
+		i = next_char(str, i, space_chars, true)
+		if str:sub(i, i) ~= ":" then
+			decode_error(str, i, "expected ':' after key")
+		end
+		i = next_char(str, i + 1, space_chars, true)
+		-- Read value
+		val, i = parse(str, i)
+		-- Set
+		res[key] = val
+		-- Next token
+		i = next_char(str, i, space_chars, true)
+		local chr = str:sub(i, i)
+		i = i + 1
+		if chr == "}" then
+			break
+		end
+		if chr ~= "," then
+			decode_error(str, i, "expected '}' or ','")
+		end
+	end
+	return res, i
+end
+
+local char_func_map = {
+	['"'] = parse_string,
+	["0"] = parse_number,
+	["1"] = parse_number,
+	["2"] = parse_number,
+	["3"] = parse_number,
+	["4"] = parse_number,
+	["5"] = parse_number,
+	["6"] = parse_number,
+	["7"] = parse_number,
+	["8"] = parse_number,
+	["9"] = parse_number,
+	["-"] = parse_number,
+	["t"] = parse_literal,
+	["f"] = parse_literal,
+	["n"] = parse_literal,
+	["["] = parse_array,
+	["{"] = parse_object,
+}
+
+parse = function(str, idx)
+	local chr = str:sub(idx, idx)
+	local f = char_func_map[chr]
+	if f then
+		return f(str, idx)
+	end
+	decode_error(str, idx, "unexpected character '" .. chr .. "'")
+end
+
+function json.decode(str)
+	if type(str) ~= "string" then
+		error("expected argument of type string, got " .. type(str))
+	end
+	local res, idx = parse(str, next_char(str, 1, space_chars, true))
+	idx = next_char(str, idx, space_chars, true)
+	if idx <= #str then
+		decode_error(str, idx, "trailing garbage")
+	end
+	return res
+end
 
 function M.saveTable(t, filename, location)
 	local loc = location
@@ -59,6 +398,8 @@ function M.loadTable(filename, location)
 	end
 end
 
+-- Helper Functions ####################################################
+
 function table.copy(t)
 	local u = {}
 	for k, v in pairs(t) do
@@ -86,10 +427,6 @@ local function arrayRemove(table, fnKeep)
 	return table
 end
 
-local render_tags_panel = ya.sync(function(state)
-	ya.dbg(state)
-end)
-
 local selected_or_hovered = ya.sync(function()
 	local tab, urls = cx.active, {}
 	for _, url in pairs(tab.selected) do
@@ -106,6 +443,64 @@ local hovered = ya.sync(function()
 	local url = cx.active.current.hovered.url
 	return url.parent .. "/" .. url.name
 end)
+
+local toggle_ui = ya.sync(function(self)
+	if self.children then
+		Modal:children_remove(self.children)
+		self.children = nil
+	else
+		self.children = Modal:children_add(self, 10)
+	end
+	ya.render()
+end)
+
+-- Plugin Methods ##########################################
+-- credit to yazi-rs/plugins/mount.yazi for the modal viewing code
+
+function M:new(area)
+	self:layout(area)
+	return self
+end
+
+function M:layout(area)
+	local chunks = ui.Layout()
+		:constraints({
+			ui.Constraint.Percentage(30),
+			ui.Constraint.Percentage(40),
+			ui.Constraint.Percentage(30),
+		})
+		:split(area)
+
+	local chunks = ui.Layout()
+		:direction(ui.Layout.HORIZONTAL)
+		:constraints({
+			ui.Constraint.Percentage(30),
+			ui.Constraint.Percentage(40),
+			ui.Constraint.Percentage(30),
+		})
+		:split(chunks[2])
+
+	self._area = chunks[2]
+end
+
+function M:redraw()
+	local tag_database = M.loadTable("tags.json")
+	local file = hovered()
+	local rows = { ui.Row(tag_database[file] or {}) }
+	return {
+		ui.Clear(self._area),
+		ui.Border(ui.Border.ALL)
+			:area(self._area)
+			:type(ui.Border.ROUNDED)
+			:style(ui.Style():fg("blue"))
+			:title(ui.Line("File Tags"):align(ui.Line.CENTER)),
+		ui.Table(rows)
+			:area(self._area:pad(ui.Pad(1, 2, 1, 2)))
+			:header(ui.Row({ "Tags" }):style(ui.Style():bold()))
+			:row(self.cursor)
+			:row_style(ui.Style():fg("blue"):underline()),
+	}
+end
 
 function M:entry(job)
 	local action = job.args[1]
@@ -174,54 +569,8 @@ function M:entry(job)
 		M.saveTable(tag_database, "tags.json")
 	elseif action == "list" then
 		-- TODO: Create a UI panel that lists the tags of the current file being hovered
-		ya.dbg("hello")
 		toggle_ui()
 	end
-end
-
-function M:new(area)
-	self:layout(area)
-	return self
-end
-
-function M:layout(area)
-	local chunks = ui.Layout()
-		:constraints({
-			ui.Constraint.Percentage(30),
-			ui.Constraint.Percentage(40),
-			ui.Constraint.Percentage(30),
-		})
-		:split(area)
-
-	local chunks = ui.Layout()
-		:direction(ui.Layout.HORIZONTAL)
-		:constraints({
-			ui.Constraint.Percentage(30),
-			ui.Constraint.Percentage(40),
-			ui.Constraint.Percentage(30),
-		})
-		:split(chunks[2])
-
-	self._area = chunks[2]
-end
-
-function M:redraw()
-	local tag_database = M.loadTable("tags.json")
-	local file = hovered()
-	local rows = { ui.Row(tag_database[file] or {}) }
-	return {
-		ui.Clear(self._area),
-		ui.Border(ui.Border.ALL)
-			:area(self._area)
-			:type(ui.Border.ROUNDED)
-			:style(ui.Style():fg("blue"))
-			:title(ui.Line("File Tags"):align(ui.Line.CENTER)),
-		ui.Table(rows)
-			:area(self._area:pad(ui.Pad(1, 2, 1, 2)))
-			:header(ui.Row({ "Tags" }):style(ui.Style():bold()))
-			:row(self.cursor)
-			:row_style(ui.Style():fg("blue"):underline()),
-	}
 end
 
 M.setup = function(state, opts) end
